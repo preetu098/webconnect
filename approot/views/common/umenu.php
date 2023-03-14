b751163cceb4d55039e0d6cca0169c8b99610463
Preetu098 <preetu098@gmail.com>
2023-03-14 16:36:41 +0530

    add changes

--- approot/views/common/umenu.php
+++ approot/views/common/umenu.php
@@ -10,6 +10,9 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <meta name="format-detection" content="telephone=no">
    <title>Employee</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
    <link rel="shortcut icon" type="image/png" href="images/favicon.png" />
    <link href="<?= base_url('external/'); ?>vendor/datatables/css/jquery.dataTables.min.css" rel="stylesheet">
    <link href="<?= base_url('external/'); ?>vendor/jquery-nice-select/css/nice-select.css" rel="stylesheet">
